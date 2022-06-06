9c8ad31b8640c51f6ee3d50b66e0cca03073600d
Fatima-Azzahraa SEDRAT <fatima.azzahraa.sedrat@gmail.com>
2022-06-06 20:40:11 +0200

    Step 5: passing test

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,5 +1,7 @@
+import java.util.Vector;
+
 public class StringCalculator {
-	int Add(String numbers) {
+	int Add(String numbers) throws Exception{
 		if (numbers.isEmpty()) {
 			return 0;
 		}
@@ -11,10 +13,25 @@ public class StringCalculator {
 			numbers = numbers.substring(4);
 		}
 
+		Vector<Integer> negativeNumbers = new Vector<Integer>();
+
 		int sum = 0;
 		for (var part : numbers.split(delimiter)) {
-			sum += Integer.parseInt(part);
+			int value = Integer.parseInt(part);
+			if (value < 0) {
+				negativeNumbers.add(value);
+			}
+			sum += value;
 		}
+
+		if (!negativeNumbers.isEmpty()) {
+			String[] arr = new String[negativeNumbers.size()];
+			for (int i = 0 ; i < arr.length ; i++) {
+				arr[i] = negativeNumbers.get(i).toString();
+			}
+			throw new Exception("negatives not allowed: " + String.join(", ", arr) + ".");
+		}
+
 		return sum;
 	}
 }
--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -4,75 +4,76 @@ import static org.junit.Assert.fail;
 
 public class StringCalculatorTest {
   @Test
-  public void returnZeroIfEmpty() {
+  public void returnZeroIfEmpty() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("");
     assertEquals(0, result);
   }
 
   @Test
-  public void returnOneIfGivenOne() {
+  public void returnOneIfGivenOne() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("1");
     assertEquals(1, result);
   }
 
   @Test
-  public void returnTwoIfGivenTwo() {
+  public void returnTwoIfGivenTwo() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("2");
     assertEquals(2, result);
   }
 
   @Test
-  public void returnTwoIfGivenOneCommaOne() {
+  public void returnTwoIfGivenOneCommaOne() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("1,1");
     assertEquals(2, result);
   }
 
   @Test
-  public void returnThirtyIfGivenTenCommaTwenty() {
+  public void returnThirtyIfGivenTenCommaTwenty() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("10,20");
     assertEquals(30, result);
   }
 
   @Test
-  public void returnThreeIfGivenOneCommaOneCommaOne() {
+  public void returnThreeIfGivenOneCommaOneCommaOne() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("1,1,1");
     assertEquals(3, result);
   }
 
   @Test
-  public void returnSixtyIfGivenTenCommaTwentyCommaThirty() {
+  public void returnSixtyIfGivenTenCommaTwentyCommaThirty() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("10,20,30");
     assertEquals(60, result);
   }
 
   @Test
-  public void returnSixIfGivenOneNewLineTwoCommaThree() {
+  public void returnSixIfGivenOneNewLineTwoCommaThree() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("1\n2,3");
     assertEquals(6, result);
   }
 
   @Test
-  public void returnThreeIfGivenCustomDelimiterAndOneDelimiterTwo() {
+  public void returnThreeIfGivenCustomDelimiterAndOneDelimiterTwo() throws Exception {
     StringCalculator sc = new StringCalculator();
     int result = sc.Add("//;\n1;2");
     assertEquals(3, result);
   }
 
   @Test
-  public void throwsExceptionIfGivenNegativeNumbers() {
+  public void throwsExceptionIfGivenNegativeNumbers() throws Exception {
     StringCalculator sc = new StringCalculator();
     try {
       int result = sc.Add("//;\n1;-2;3;-4");
     } catch (Exception e) {
       assertEquals("negatives not allowed: -2, -4." ,e.getMessage());
+      return ;
     }
     fail("StringCalculator Add method did not throw exception on negative numbers.");
   }
