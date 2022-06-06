edb549bd350df470d4e5bf0bace9e67b953b4586
Fatima-Azzahraa SEDRAT <fatima.azzahraa.sedrat@gmail.com>
2022-06-06 20:40:11 +0200

    Step 1: additional passing test

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -29,4 +29,11 @@ public class StringCalculatorTest {
     int result = sc.Add("1,1");
     assertEquals(2, result);
   }
+
+  @Test
+  public void returnThirtyIfGivenTenCommaTwenty() {
+    StringCalculator sc = new StringCalculator();
+    int result = sc.Add("10,20");
+    assertEquals(30, result);
+  }
 }
