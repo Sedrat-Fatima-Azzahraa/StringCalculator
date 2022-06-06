78559f63c13d9828e3d82fc91251ceec75dfa70a
Fatima-Azzahraa SEDRAT <fatima.azzahraa.sedrat@gmail.com>
2022-06-06 20:40:11 +0200

    Step 1: refactor

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,11 +3,15 @@ public class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		if (numbers.indexOf(",") == -1) {
+
+		int commaIndex = numbers.indexOf(",");
+		
+		if (commaIndex == -1) {
 			return Integer.parseInt(numbers);
 		}
-		String left = numbers.substring(0, numbers.indexOf(","));
-		String right = numbers.substring(numbers.indexOf(",") + 1);
+
+		String left = numbers.substring(0, commaIndex);
+		String right = numbers.substring(commaIndex + 1);
 
 		return Integer.parseInt(left) + Integer.parseInt(right);
 	}
